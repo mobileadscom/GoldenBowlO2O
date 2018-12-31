1c53a4c4d7793a2b91c8dca47e76db08f7f6175c
yklim <yk@mobileads.com>
2018-12-31 17:27:26 +0800

    tnc and page layout update

--- public/javascripts/index.js
+++ public/javascripts/index.js
@@ -108,14 +108,6 @@ var app = {
 		}	
 	},
 	continue: function() {
-		// this.changeHeaderImage();
-
-
-		
-
-		/*apply answer to answered question */
-
-
 		if (user.info.state == 'win') {
 			this.initResult('win', user.info.couponLink);
 			this.pages.toPage('resultPage');
@@ -130,141 +122,39 @@ var app = {
 	},
 	events: function() {
 		/* ==== Event Listeners ==== */
-	  /* enabled terms agree checkbox when scrolled tnc to bottom */
-	 /* var enableAgreeCheckbox = false;
-	  document.getElementById('tnc').addEventListener('scroll', function(event) {
-	  	if (!enableAgreeCheckbox) {
-	  		var element = event.target;
-		    if (element.scrollHeight - element.scrollTop < element.clientHeight + 50) {
-		    	document.getElementById('startSurvey').disabled = false;*/
-		      /*document.getElementById('agreeCheck').disabled = false;
-		      enableAgreeCheckbox = true;*/
-		 //    }
-	  // 	}
-	  // });
-	  
-	  /* enable start survey button when terms agree checkbox is checked */
-	  document.getElementById('agreeCheck').onchange = function() {
-	    if (this.checked) {
-				document.getElementById('startSurvey').disabled = false;
-	    }
-	    else {
-	    	document.getElementById('startSurvey').disabled = true;
-	    }
-	  }
-	  
-	  /* Finished Answering Questions, process result */
-	  /*var processed = false;
-	  document.getElementById('toResult').addEventListener('click', () => {
-	  	if (!processed) {
-	  		processed = true;
-	  		this.processResult();
-	  	}
-	  });*/
-
-			/* email registration */
-			var form = document.getElementById('regForm');
-			form.onsubmit = (event) => {
-				var spinner = document.getElementById('formWorking');
-				var regBtn = document.getElementById('submitBtn');
-				var backBtn = document.getElementById('toRegSub1');
-				regBtn.style.display = 'none';
-				backBtn.style.display = 'none';
-				spinner.style.display = 'block';
-				event.preventDefault();
-				var email = document.getElementById('emailInput').value;
-				user.register(email).then((response) => {
-					console.log(response);
-					spinner.style.display = 'none';
-				// if (response.data.status == true) {
-						this.formSections.toPage('doneSec');
-						var emailContent = '<head><meta charset="utf-8"></head>Thank you for registering. Please click the link below to complete your registration and join the campaign.<br><br><a href="https://demo.o2oplatform.com/scratch/?userId=' + email + '" target="_blank">https://demo.o2oplatform.com/scratch/?userId=' + email + '</a>';
-						user.sendEmail(email, 'MobileAds O2O Demo Link', emailContent);
-						// user.trackRegister();
-				//  }
-				//   else if (response.data.message == 'user exist.') {
-					//   user.info = response.data.user;
-				//    	 this.continue();
-						// modal.closeAll();
-				//  }
-				}).catch((error) => {
-					console.log(error);
-					regBtn.style.display = 'block';
-					backBtn.style.display = 'block';
-					spinner.style.display = 'none';
-				});
-			};
-
-    /* twitter registration / login */
-    var twitReg = document.getElementById('regTwitter');
-    twitReg.onclick = () => {
-      var regLoader = document.getElementById('regWorking');
-      var regButtons = document.getElementById('regButtons');
-      regLoader.style.display = 'block';
-      regButtons.style.display = 'none';
-			user.registerTwitter().then((result) => {
-        // This gives you a the Twitter OAuth 1.0 Access Token and Secret.
-        // You can use these server side with your app's credentials to access the Twitter API.
-        user.twitter.token = result.credential.accessToken;
-        user.twitter.secret = result.credential.secret;
-        var twitterId = result.additionalUserInfo.profile.id_str;
-        this.initUser(twitterId, true, true);
-      }).catch((error) => {
-      	regLoader.style.display = 'none';
-        regButtons.style.display = 'block';
-        // Handle Errors here.
-        var errorCode = error.code;
-        var errorMessage = error.message;
-        // The email of the user's account used.
-        var email = error.email;
-        // The firebase.auth.AuthCredential type that was used.
-        var credential = error.credential;
-        alert(errorMessage);
-        // ..
-      });
-    };
-
-    var followBtn = document.getElementById('followBtn');
-    followBtn.onclick = () => {
-    	followBtn.style.display = 'none';
-    	user.followTwitter().then((response) => {
+		/* email registration */
+		var form = document.getElementById('regForm');
+		form.onsubmit = (event) => {
+			var spinner = document.getElementById('formWorking');
+			var regBtn = document.getElementById('submitBtn');
+			var backBtn = document.getElementById('toRegSub1');
+			regBtn.style.display = 'none';
+			backBtn.style.display = 'none';
+			spinner.style.display = 'block';
+			event.preventDefault();
+			var email = document.getElementById('emailInput').value;
+			user.register(email).then((response) => {
 				console.log(response);
-	        if (response.data == 'followed!') {
-	          var sMsg = document.getElementById('successFollow');
-	          sMsg.style.display = 'block';
-	          setTimeout(() => {
-	            this.continue();
-	          }, 2000);
-	        }
-    	}).catch((error) => {
+				spinner.style.display = 'none';
+			// if (response.data.status == true) {
+					this.formSections.toPage('doneSec');
+					var emailContent = '<head><meta charset="utf-8"></head>Thank you for registering. Please click the link below to complete your registration and join the campaign.<br><br><a href="https://demo.o2oplatform.com/scratch/?userId=' + email + '" target="_blank">https://demo.o2oplatform.com/scratch/?userId=' + email + '</a>';
+					user.sendEmail(email, 'MobileAds O2O Demo Link', emailContent);
+					// user.trackRegister();
+			//  }
+			//   else if (response.data.message == 'user exist.') {
+				//   user.info = response.data.user;
+			//    	 this.continue();
+					// modal.closeAll();
+			//  }
+			}).catch((error) => {
 				console.log(error);
-				followBtn.style.display = 'block';
-    	});
-    }
-
-    document.getElementById('toVideo').addEventListener('click', () => {
-			setTimeout(() => {
-				this.player.playVideo();
-			}, 300);
-    });
-	  /* ==== Event Listeners End ==== */
-	},
-	checkTwitter: function() { // Check if user is following official page
-		user.isFollowingTwitter().then((resp) => {
-      console.log(resp);
-      if (resp.data == 'following') {
-				this.continue();
-      }
-      else {
-		     this.pages.toPage('followPage');
-		     // this.changeHeaderImage();
-      }
-    // this.continue();
-    }).catch((error) => {
-      console.log(error);
-      document.getElementById('regWorking').style.display = 'none';
-      document.getElementById('regButtons').style.display = 'block';
-    });
+				regBtn.style.display = 'block';
+				backBtn.style.display = 'block';
+				spinner.style.display = 'none';
+			});
+		};
+		/* ==== Event Listeners End ==== */
 	},
 	initUser: function(userId, autoRegister, isTwitter) {
 		/* check if user is registered, if no, then register user, if yes, continue on where the user left off */
@@ -277,12 +167,7 @@ var app = {
 						user.isWanderer = false;
 						user.info.id = userId;
 						user.source = this.params.source;
-						if (isTwitter) {
-							this.checkTwitter();
-						}
-						else {
-							this.continue();
-						}
+						this.continue();
 					  // user.trackRegister();
 	    		}).catch((err) => {
 	    			user.isWanderer = true;
@@ -300,13 +185,7 @@ var app = {
     		user.isWanderer = false;
 				user.info = response.data.user;
 				user.source = this.params.source;
-				
-				if (isTwitter) {
-					this.checkTwitter();
-				}
-				else {
-					this.continue();
-				}
+				this.continue();
     	}
     }).catch((error) => {
     	user.isWanderer = true;
@@ -341,9 +220,6 @@ var app = {
 		})
 	},
 	init: function() {
-		var vidWidth = document.getElementById('vid').clientWidth;
-	    var vidHeight = document.getElementById('vid').clientHeight;
-
 		/* init pagination */
 		this.params = this.getParams();
 		if (this.params.displayName) {
@@ -398,55 +274,7 @@ var app = {
 	}
 	  
 	  var processed = false; // check if result has been processed to avoid double result processsing
-
-		//youtube api
-    var ytScript = document.createElement('script');
-    ytScript.src = "https://www.youtube.com/iframe_api";
-    var firstScriptTag = document.getElementsByTagName('script')[0];
-    firstScriptTag.parentNode.insertBefore(ytScript, firstScriptTag);
-    
-    window.onYouTubeIframeAPIReady = () => {
-      this.player = new YT.Player('vid', {
-        height: vidHeight.toString(),
-        width: vidWidth.toString(),
-        playerVars: {'rel': 0,'showinfo': 0, 'controls': 0, 'playsinline': 1},
-        videoId: '6kpZW8oQ9tw',
-        events: {
-          'onStateChange': (event) => {
-            if (event.data == YT.PlayerState.ENDED) {
-            	/*if (!processed) {
-	            	processed = true;
-	            	this.processResult();
-								this.pages.toPage('resultPage');
-							}*/
-				this.pages.toPage('page2');
-            }
-            else if (event.data == YT.PlayerState.PLAYING) {
-            	var playtimer = setInterval(() => {
-	        		if (this.player.getPlayerState() != 1) {
-	        			console.log('videoEnded!');
-	        			clearInterval(playtimer);
-	        		}
-	        		else {
-	        			var percentage = (this.player.getCurrentTime() / this.player.getDuration() * 100).toFixed(0)
-		            	document.getElementById('vidProgress').style.width = percentage + '%'
-	        		}
-	        	}, 500);
-            	
-				
-            }
-          }
-        }
-      });
-    }
 	},
-	/*changeHeaderImage() {
-    	document.getElementById('mainBanner').style.display = 'none';
-    	var x = window.matchMedia("(min-width: 992px)");
-    	if (!x.matches) {
-			document.getElementById('banner').style.display = 'block';
-    	}
-	}*/
 }
 
 document.addEventListener('DOMContentLoaded', function() {
